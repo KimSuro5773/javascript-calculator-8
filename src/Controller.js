7ffbd668fb96530bbe469704c3d7f5c7d7b04ead
Kimsuro <ss01056489656@gmail.com>
2025-10-19 02:09:24 +0900

    docs(Controller): 계산 흐름 주석에 예시 데이터 추가

--- src/Controller.js
+++ src/Controller.js
@@ -12,19 +12,19 @@ class Controller {
 
   async run() {
     try {
-      // 1. 사용자 입력 받기 (View)
+      // 1. 사용자 입력 받기  ex) userInput : //&\n1&2
       const userInput = await this.#view.getUserInput();
 
-      // 2. 구분자 파싱 (Model)
+      // 2. 구분자 파싱  ex) calculationString : 1&2 | delimiterArray : [",", ":", "&"]
       const { calculationString, delimiterArray } = this.#model.delimiterParse(userInput);
 
-      // 3. 문자열을 구분자로 분리 후 배열로 변환 (Model)
+      // 3. 문자열을 구분자로 분리 후 배열로 변환  ex) numbers : ["1", "2"]
       const numbers = this.#model.delimiterSplit(calculationString, delimiterArray);
 
-      // 계산 (Model)
+      // 4. 계산 ex) result : 3
       const result = this.#model.calculate(numbers);
 
-      // 결과 출력 (View)
+      // 5. 결과 출력
       this.#view.printResult(result);
     } catch (error) {
       throw error;
