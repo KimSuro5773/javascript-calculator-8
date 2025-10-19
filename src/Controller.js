f39f97226b7ece5091da20e0d0f14eb6aa3f58d9
Kimsuro <ss01056489656@gmail.com>
2025-10-19 12:24:01 +0900

    refactor(Controller): 불필요한 try-catch 블록 제거

--- src/Controller.js
+++ src/Controller.js
@@ -11,24 +11,20 @@ class Controller {
   }
 
   async run() {
-    try {
-      // 1. 사용자 입력 받기  ex) userInput : //&\n1&2
-      const userInput = await this.#view.getUserInput();
+    // 1. 사용자 입력 받기  ex) userInput : //&\n1&2
+    const userInput = await this.#view.getUserInput();
 
-      // 2. 구분자 파싱  ex) calculationString : 1&2 | delimiterArray : [",", ":", "&"]
-      const { calculationString, delimiterArray } = this.#model.delimiterParse(userInput);
+    // 2. 구분자 파싱  ex) calculationString : 1&2 | delimiterArray : [",", ":", "&"]
+    const { calculationString, delimiterArray } = this.#model.delimiterParse(userInput);
 
-      // 3. 문자열을 구분자로 분리 후 배열로 변환  ex) numbers : ["1", "2"]
-      const numbers = this.#model.delimiterSplit(calculationString, delimiterArray);
+    // 3. 문자열을 구분자로 분리 후 배열로 변환  ex) numbers : ["1", "2"]
+    const numbers = this.#model.delimiterSplit(calculationString, delimiterArray);
 
-      // 4. 계산 ex) result : 3
-      const result = this.#model.calculate(numbers);
+    // 4. 계산 ex) result : 3
+    const result = this.#model.calculate(numbers);
 
-      // 5. 결과 출력
-      this.#view.printResult(result);
-    } catch (error) {
-      throw error;
-    }
+    // 5. 결과 출력
+    this.#view.printResult(result);
   }
 }
 
