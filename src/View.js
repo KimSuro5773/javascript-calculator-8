003a3adad8776348c99b55798efdf9ff8c8afcba
Kimsuro <ss01056489656@gmail.com>
2025-10-20 22:07:24 +0900

    refactor(View): 불필요한 return await 중복 제거

--- src/View.js
+++ src/View.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 class View {
   async getUserInput() {
-    return await Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
+    return Console.readLineAsync('덧셈할 문자열을 입력해 주세요.\n');
   }
 
   printResult(result) {
